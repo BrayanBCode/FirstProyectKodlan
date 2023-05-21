a1a1db05c3f76d9febb265846a3dd1e113fae147
BrayanBCode <134159765+BrayanBCode@users.noreply.github.com>
2023-05-20 22:16:56 -0300

    Update Diccionario.py

--- Diccionario.py
+++ Diccionario.py
@@ -1,7 +1,9 @@
 meme_dict = {
     "CRINGE": "Algo excepcionalmente raro o embarazoso",
     "LOL": "Una respuesta común a algo gracioso",
-    "TOP DIF": "Diferencia de toplaner (League of leguens)"
+    "TOP DIF": "Diferencia de toplaner (League of leguens)",
+    "XD": "representa una cara riéndose con la boca abierta y los ojos cerrados",
+    "UWU": "se emplea para expresar felicidad o ternura"
 }
 
 
